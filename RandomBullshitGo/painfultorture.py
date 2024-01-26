632fd7b6a9b78f489aa2fed2907ed8dc07466eb3
WillMcBev <151534829+WillMcBev@users.noreply.github.com>
2024-01-26 15:02:22 +0000

    Update painfultorture.py

--- RandomBullshitGo/painfultorture.py
+++ RandomBullshitGo/painfultorture.py
@@ -71,6 +71,7 @@ def forward():
         
         if left_enc.position > right_enc.position:
             motorR.duty_cycle = 30000
+            motorL.duty_cycle = 0
         elif right_enc.position > left_enc.position:
             motorR.duty_cycle = 0
             motorL.duty_cycle = 30000
@@ -91,9 +92,10 @@ def smallforward():
     timeout = time.time() + 0.3
     while time.time() < timeout:
         
-        if left_enc.position > right_enc.position and error != 0:
+        if left_enc.position > right_enc.position:
             motorR.duty_cycle = 30000
-        elif right_enc.position > left_enc.position and error != 0:
+            motorL.duty_cycle = 0
+        elif right_enc.position > left_enc.position:
             motorR.duty_cycle = 0
             motorL.duty_cycle = 30000
         else:
