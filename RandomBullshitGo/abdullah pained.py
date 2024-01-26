c5902556d32b38905ced62e898abfc0480ae2270
aa3235 <150688575+aa3235@users.noreply.github.com>
2024-01-26 15:28:57 +0000

    Add files via upload

---
+++ RandomBullshitGo/abdullah pained.py
@@ -0,0 +1,143 @@
+import board
+import time
+import analogio
+import digitalio
+import busio
+import pwmio
+import adafruit_tca9548a
+import adafruit_vl6180x
+
+runtime = True
+stage = 1
+# Sensor SETUP
+
+i2c = busio.I2C(board.GP15, board.GP14) # uses board.SCL and board.SDA
+mux = adafruit_tca9548a.TCA9548A(i2c) # make tsa (multiplexer) object using i2c
+
+Rsensor = adafruit_vl6180x.VL6180X(mux[5])
+Fsensor = adafruit_vl6180x.VL6180X(mux[7])
+Lsensor = adafruit_vl6180x.VL6180X(mux[6])
+
+print('R Range: {0}mm'.format(Rsensor.range))
+print('F Range: {0}mm'.format(Fsensor.range))
+print('L Range: {0}mm'.format(Lsensor.range))
+
+#MOTOR SETUP
+
+motorR = pwmio.PWMOut(board.GP2, frequency = 1000)
+
+d1 = digitalio.DigitalInOut(board.GP1)
+d1.direction = digitalio.Direction.OUTPUT
+d2 = digitalio.DigitalInOut(board.GP0)
+d2.direction = digitalio.Direction.OUTPUT
+
+motorL = pwmio.PWMOut(board.GP5, frequency = 1000)
+
+d3 = digitalio.DigitalInOut(board.GP4)
+d3.direction = digitalio.Direction.OUTPUT
+d4 = digitalio.DigitalInOut(board.GP3)
+d4.direction = digitalio.Direction.OUTPUT
+
+d1.value = 0
+d2.value = 1
+
+d3.value = 0
+d4.value = 1
+
+runtime = True
+
+def turnspeed():
+    motorR.duty_cycle = 20000
+    motorL.duty_cycle = 20000
+
+def motorflat():
+    motorR.duty_cycle = 0
+    motorL.duty_cycle = 0
+
+def forward():
+    d1.value = 0
+    d2.value = 1
+
+    d3.value = 0
+    d4.value = 1
+    motorR.duty_cycle = 30000
+    motorL.duty_cycle = 30000
+    
+def buffer():
+    forward()
+    time.sleep(1)
+    motorflat()
+    
+def rightturn():
+    motorflat()
+    d1.value = 1
+    d2.value = 0
+
+    d3.value = 0
+    d4.value = 1
+    turnspeed()
+    time.sleep(0.5)
+    motorflat()
+
+
+def leftturn():
+    motorflat()
+    d1.value = 0
+    d2.value = 1
+
+    d3.value = 1
+    d4.value = 0
+    turnspeed()
+    time.sleep(0.5)
+    motorflat()
+
+def sensorread():
+    print('R Range: {0}mm'.format(Rsensor.range))
+    print('F Range: {0}mm'.format(Fsensor.range))
+    print('L Range: {0}mm'.format(Lsensor.range))
+
+def perry():
+    error = 30 - Lsensor.range
+    doof = error * 100
+    motorR.duty_cycle = 30000 - doof
+    motorL.duty_cycle = 30000 + doof
+    
+def smallbuffer():
+    forward()
+    time.sleep(0.3)
+    motorflat()
+
+def turnbuffer():
+    forward()
+    time.sleep(0.4)
+    motorflat()
+
+while runtime == True:
+    forward()
+    perry()
+    if Lsensor.range >= 70:
+        motorflat()
+        time.sleep(0.5)
+        smallbuffer()
+        time.sleep(0.5)
+        leftturn()
+        time.sleep(0.5)
+        turnbuffer()
+    elif Fsensor.range <= 50:
+        motorflat()
+        if Lsensor.range >= 70:
+            time.sleep(0.5)
+            leftturn()
+            time.sleep(0.5)
+            turnbuffer()
+        elif Rsensor.range >= 70:
+            time.sleep(0.5)
+            rightturn()
+            time.sleep(0.5)
+            turnbuffer()
+        else:
+            motorflat()
+            rightturn()
+            time.sleep(0.5)
+            rightturn()
+            time.sleep(0.5)
